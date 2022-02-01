9db075351204efb6bfe3d7b1f0b503ad98227860
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-02-01 12:37:28 +0800

    Update zeeoneofc.js

--- zeeone/zeeoneofc.js
+++ zeeone/zeeoneofc.js
@@ -117,7 +117,7 @@ const download = (prefix) => {
 `
 }
 const allmenu = (kyun, os, prefix, wita, wit, ucapannya2, timuu, status, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, alfa , alfa1, thisDay, ini_tanggal, totalchat, hit_today, ini_gcchat, latensii)  => {
-	return`❏───「 *WHATSAPP BOT* 」───❏
+	return`❏───「 *Takanashi Rikka* 」───❏
                                
 ${ucapannya2}
 ⏰ Jam : ${timuu} Server Time
@@ -958,7 +958,7 @@ ${numberik} ${prefix}yuzuki
 ${numberik} ${prefix}akame
 ${numberik} ${prefix}mikosiba
 
-❏───「 *WHATSAPP BOT* 」───❏
+❏───「 *Takanashi Rikka* 」───❏
 `
 }
 
