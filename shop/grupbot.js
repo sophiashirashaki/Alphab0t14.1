edfa11ee6fd0de489b9051308b1d0dbdeee99f0a
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-02-01 14:33:46 +0800

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,10 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *Rikka Support*
+_https://chat.whatsapp.com/FIDjvBgVzx4B91T9cOpmuz_
+2. *Rikka Support 2*
+_https://chat.whatsapp.com/BFuprvfRQJkKa4sz3r1G4M_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
